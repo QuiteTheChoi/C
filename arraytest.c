39e68e154d1e8018dded69923495811ca2bfcd3a
QuiteTheChoi <jcaverly92@gmail.com>
2015-06-23 22:18:18 -0400

    Loop

--- arraytest.c
+++ arraytest.c
@@ -3,10 +3,12 @@
 int main()
 {
 	int array[10],i;
-	for (i = 0; i <=10 ; i++)
+	
+	for (i = 0; i <=11 ; i++)
 	{
 		array[i]=0; /*code should never terminate*/
-		
+		printf("test \n");
+			
 	}
 	printf("%d \n", sizeof(array)/sizeof(int));
 	return 0;
