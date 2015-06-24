d56010b91a072975f4f06061b67c1ba15e679c0d
QuiteTheChoi <jcaverly92@gmail.com>
2015-06-23 23:19:02 -0400

    Debugging

--- arraytest.c
+++ arraytest.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
-	int array[10],i;
-	
-	for (i = 0; i <=11 ; i++)
-	{
-		array[i]=0; /*code should never terminate*/
-		printf("test \n");
-			
-	}
-	printf("%d \n", sizeof(array)/sizeof(int));
-	return 0;
-}
-	
+int main() 
+{ 
+  int array[10],i; 
+
+  printf ("array: %p, &i: %p\n"); 
+  printf ("i is offset %d from array\n", &i - array);
+
+  for (i = 0; i <=11 ; i++) 
+  { 
+    printf ("%d: Writing 0 to address %p\n", i, &array[i]); 
+    array[i]=0; /*code should never terminate*/ 
+  } 
+  return 0; 
+} 
