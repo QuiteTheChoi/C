7d2d729ddf1d635e58a1d3ff057d039299b3cad8
QuiteTheChoi <jcaverly92@gmail.com>
2015-07-02 23:15:50 -0400

    Working

--- tokenizer.c
+++ tokenizer.c
@@ -9,22 +9,24 @@
  * Tokenizer type.  You need to fill in the type as part of your implementation.
  */
 
-enum states {
-	undetermined,
-	malformed,
-	int_float,
+enum states {				
+	undetermined,			//The begining of a token
+	malformed,				
+	int_float,				//final state (Integer) if no malformed char or '.' or 'e' is found. 
 	oct_hex_float_zero,
-	mightBeOct,
-	mightBeHex,
-	mightBeFloat,
-	mightBeFloat_onlyints_neg_pos,
-	mightBeFloat_onlyints,
+	mightBeOct,				//Is the final state if no malformed character is found.
+	mightBeHex, 			//Is the final state if no malformed character is found.
+	mightBeFloat,			//Is the final state if no malformed character is found, state can change if an 'e' is found
+	mightBeFloat_onlyints_neg_pos, 
+	mightBeFloat_onlyints,   //Is the final state if no malformed character is found after a negative or positive is found
 } curr_State;
 
+char err;
+
 struct TokenizerT_ {
-	int i;   			/*Index number used later for creating tokens*/
-	int stringSize;		/*Stri	ng size for getnexttoken function*/	
-	char *myString;		/*input string*/
+	int i;   			//Index number used later for creating tokens
+	int stringSize;		//Stri	ng size for getnexttoken function	
+	char *myString;		//input string
 };
 
 typedef struct TokenizerT_ TokenizerT;
@@ -44,10 +46,10 @@ typedef struct TokenizerT_ TokenizerT;
  */
 
 TokenizerT *TKCreate( char * ts ) {
-	char copy[strlen(ts)]; 					/*copy of string ts*/
+	char copy[strlen(ts)]; 					//copy of string ts
 	strcpy(copy,ts);  
 	
-	TokenizerT *ptr = (TokenizerT*) malloc (sizeof(struct TokenizerT_));   /*Allocation of size for TokenizerT_*/
+	TokenizerT *ptr = (TokenizerT*) malloc (sizeof(struct TokenizerT_));   //Allocation of size for TokenizerT_
 	ptr -> i = 0;		
 	ptr -> stringSize = strlen(copy);	
 	ptr -> myString = strdup(copy);
@@ -63,7 +65,7 @@ TokenizerT *TKCreate( char * ts ) {
  */
 
 void TKDestroy( TokenizerT * tk ) {
-	free ((void*)tk);
+	free (tk);
 }
 
 /*
@@ -104,7 +106,7 @@ char *TKGetNextToken( TokenizerT * tk ) {
 	int tokenSize = end - start;
 	tk -> i = end + 1;
 		
-	char *token = (char*) malloc ((tokenSize+2)*sizeof(char));
+	char *token = (char*) malloc ((tokenSize+2)*sizeof(char));   
 	
 	int i;
 	for (i = 0; i < tokenSize; i++) {
@@ -116,12 +118,21 @@ char *TKGetNextToken( TokenizerT * tk ) {
 		
 }
 
-/*beginning of all states and function calls for states*/
+//beginning of all states and function calls for states
+
+/*set the global char to the malform char of token*/
+int malChar(char ptr) {
+	err = ptr;
+	return 0;
+}
+
+/* this will check whether or not the first character of token is a valid char of 
+ * (1-9), '-' (this will be int or float), '0' (which will be float, hex, oct or zero state) or a malformed character. */
 int isZeroOrNot(char x) {
 	if (x == '0') {
 		curr_State = oct_hex_float_zero;
 	}
-	else if (x >= '0' && x <= '9') {
+	else if ((x > '0' && x <= '9')||(x=='-')) {
 		curr_State = int_float;
 	}
 	else if (x == '.') {
@@ -129,73 +140,16 @@ int isZeroOrNot(char x) {
 	}
 	else {
 		curr_State = malformed;
+		malChar(x);
 	}
 	
 	return 0;
 }
 
-int isOctal(char x) {
-	if (x >= '0' && x <= '7') {
-	}
-	else
-		curr_State = malformed;
-	return 0;
-}
-
-int isHex(char x) {
-	if ((x >= '0' && x <= '9') || (x >= 'a' && x <= 'f') || (x >= 'A' && x <= 'F')) {
-		}
-	else
-		curr_State = malformed;
-	return 0;
-}
-
-int isDigit(char x) {
-	if (x >= '0' && x <= '9') {
-	}
-	else
-		curr_State = malformed;
-	return 0;
-}
-
-int isDigitDotOrE (char x) {
-	if (x == '.') {
-		curr_State = mightBeFloat;
-	}
-	else if (x == 'e' || x == 'E') {
-		curr_State = mightBeFloat_onlyints_neg_pos;
-	}
-	else if (x >= '0' && x <= '9') {
-	}
-	else {
-		curr_State = malformed;
-	}
-	return 0;
-}
-
-int isDigitOrE(char x) {
-	if (x >= '0' && x <= '9') {
-		curr_State = mightBeFloat;
-	}
-	else if (x == 'e' || x == 'E'){
-		curr_State = mightBeFloat_onlyints_neg_pos;
-	}
-	else {
-		curr_State = malformed;
-	}
-	return 0;
-}
-
-int isDigitOrSign(char x) {
-	if ((x >= '0' && x <= '9') || (x == '-' || x == '+')) {
-		curr_State	=  mightBeFloat_onlyints;
-	}
-	else {
-	curr_State = malformed;
-	}
-	return 0;
-}
-
+/* if isZeroOrNot determines that it could be a hex, float, oct or zero, 
+ * this will determine if the next char is a 'x' or 'X' for hex, (0-7) for oct
+ * or '.' for float and malform for all other characters. 'e' will not make it a 
+ * float as the previous state was determined from * a '0', "0e" should not be valid. */
 int isOctHexOrFloat(char x) {
 	if (x >= '0' && x <= '7') {
 		curr_State = mightBeOct;
@@ -208,22 +162,102 @@ int isOctHexOrFloat(char x) {
 	}
 	else {
 		curr_State = malformed;
+		malChar(x);
 	}
 	
 	return 0;
 }
 
-int isMal(){
-	return 0;    /*temporary*/
+/*This will change the state from mightBeOct to malform
+ * if anything besides (0-7) is found*/
+int isOctal(char x) {				
+	if (x >= '0' && x <= '7') {
+	}
+	else {
+		curr_State = malformed;
+		malChar(x);
+	}
+	return 0;
 }
 
+/*This will change the state from mightBeHex to malform
+ * if anything besides (0-9), (a-f) or (A-F) is found is found*/
+int isHex(char x) {
+	if ((x >= '0' && x <= '9') || (x >= 'a' && x <= 'f') || (x >= 'A' && x <= 'F')) {
+		}
+	else {
+		curr_State = malformed;
+		malChar(x);
+	}
+	return 0;
+}
+
+/*This is for a float that consists of an 'e' and '+' or '-', the only thing left that 
+ * should be found is integers, if not, malform state*/
+int isDigit(char x) {
+	if (x >= '0' && x <= '9') {
+	}
+	else {
+		curr_State = malformed;
+		malChar(x);
+	}
+	return 0;
+}
+
+/*This will determine if a token is an integer or float, the 
+ * possible values for integer are (0-9), '.', 'e', 'E' will change the 
+ * state appropriately */
+int isDigitDotOrE (char x) {
+	if (x == '.') {
+		curr_State = mightBeFloat;
+	}
+	else if (x == 'e' || x == 'E') {
+		curr_State = mightBeFloat_onlyints_neg_pos;
+	}
+	else if (x >= '0' && x <= '9') {
+	}
+	else {
+		curr_State = malformed;
+		malChar(x);
+	}
+	return 0;
+}
+
+/* This is called after a token is determined to be a float 
+ * because a '.' has been found, only digits and 'e' can be found after the first '.' */
+int isDigitOrE(char x) {
+	if (x >= '0' && x <= '9') {
+		curr_State = mightBeFloat;
+	}
+	else if (x == 'e' || x == 'E'){
+		curr_State = mightBeFloat_onlyints_neg_pos;
+	}
+	else {
+		curr_State = malformed;
+		malChar(x);
+
+	}
+	return 0;
+}
+
+/* This is called after a possible float token has an 'e'
+ * the only possible char after this e is digits or a negative or positive sign */
+int isDigitOrSign(char x) {
+	if ((x >= '0' && x <= '9') || (x == '-' || x == '+')) {
+		curr_State	=  mightBeFloat_onlyints;
+	}
+	else {
+		curr_State = malformed;
+		malChar(x);
+	}
+	return 0;
+}
 
 int IdentifyToken (char *ptr) {
 	while (*ptr != '\0') {
 		switch(curr_State) {
 			case (malformed): {     /*temporary*/
-				isMal();
-				break;
+				return 0;
 			}
 			case(undetermined): {
 				isZeroOrNot(*ptr);
@@ -313,12 +347,11 @@ char* TKGetState() {
  */
 
 int main(int argc, char **argv) {
-	if (argc == 1 || argv[1][0] == '\0') {									/*in case no arguments are given*/
-		fprintf(stdout,"No arguments given!\n");
+	if (argc == 1 || argv[1][0] == '\0') {									//in case no arguments are given
 		return 0;
 	}
 		
-	TokenizerT *tokenizer = TKCreate (argv[1]);        /*creation of tokenizerT*/
+	TokenizerT *tokenizer = TKCreate (argv[1]);        //creation of tokenizerT
 	
 	curr_State = undetermined;
 	char *token =TKGetNextToken(tokenizer);
@@ -327,13 +360,18 @@ int main(int argc, char **argv) {
 	while (token != 0) { 
 		IdentifyToken(token);
 		status = TKGetState();
-		fprintf(stdout,"%s: %s\n",status,token);
+		fprintf(stdout,"%s: %s ",status,token);
+		if (curr_State == malformed) {
+			fprintf(stdout,"Error caused by: [0x%x]\n",err);
+		}
+		else
+			fprintf(stdout,"\n");
 		curr_State = undetermined;
-		free(token);
 		token = TKGetNextToken(tokenizer);
 	}
 	
+	free (token);
 	TKDestroy(tokenizer);
-			
+				
 	return 0;
 }
