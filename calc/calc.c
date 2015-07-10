194d4701bcb75bee97da08fe09ae9b3fd64598f2
QuiteTheChoi <jcaverly92@gmail.com>
2015-07-10 16:55:42 -0400

    almost done with calc

--- calc/calc.c
+++ calc/calc.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
 /*struct for creating a number*/
 struct number_ {
@@ -46,6 +48,25 @@ int isOctal(char* numO) {
 int isHex (char* numH) {
 	while (*numH != '\0') {
 		switch(curr_State) {
+			case(mightBeHex): {
+				if ((*numH >= '0' && *numH <= '9')|| (*numH >= 'a' && *numH <= 'f') || (*numH >= 'A' && *numH <= 'F'))  {
+					break;
+				}
+				else
+				return 1;
+			}
+		}
+		numH++;
+	}
+	if (curr_State == mightBeHexFirstChar || curr_State == mightBeHexSecChar || curr_State == mightBeHexThirdChar)
+		return 1;
+	
+	return 0;
+}
+
+/*int isHex (char* numH) {
+	while (*numH != '\0') {
+		switch(curr_State) {
 			case(mightBeHexFirstChar): {
 				if (*numH == '0') {
 					curr_State = mightBeHexSecChar;
@@ -84,7 +105,7 @@ int isHex (char* numH) {
 		return 1;
 	
 	return 0;
-}
+}*/
 
 int isDecimal(char* numD) {
 	while (*numD != '\0') {
@@ -93,6 +114,9 @@ int isDecimal(char* numD) {
 				if (*numD >= '1' && *numD <= '9') {
 					curr_State = mightBeDec;
 					break;
+				}
+				else if ((int)strlen(numD) == 1 && isdigit(*numD)) {
+					return 0;
 				}
 				else
 					return 1;
@@ -116,21 +140,21 @@ int isDecimal(char* numD) {
  * Returns a 1 if number entered is found to be an invalid format. */
 int validateToken(char* num) {
 	char tempType;
-	char tempNum[strlen(num)];
+	char* tempNum;
 	
 	if (num[0] == '-' && strlen(num) <= 2) {         	//to have a negative sign means you must have at least a '-', a type (b,o,x,d) 
 		return 1;										// and at least one digit.
 	}
 	else if (num[0] == '-') {
 		tempType = num[1];
-		strncpy(tempNum,&num[2],strlen(num)-2);
+		tempNum = strdup(&num[2]);
 	}
 	else if (strlen(num) == 1) {						//string must contain a type and at least one digit
 		return 1;
 	}
 	else {
 		tempType = num[0];
-		strncpy(tempNum,&num[1],strlen(num)-1);
+		tempNum = strdup(&num[1]);
 	}
 
 	if (tempType == 'b')
@@ -145,7 +169,7 @@ int validateToken(char* num) {
 	}
 	else if (tempType == 'x')
 	{
-		curr_State = mightBeHexFirstChar;
+		curr_State = mightBeHex;
 		return isHex(tempNum);
 	}
 	else if (tempType == 'd')
@@ -154,12 +178,157 @@ int validateToken(char* num) {
 		return isDecimal(tempNum);		
 	}
 	curr_State = undetermined;
+	free(tempNum);
 	return 1;
+}
+
+int strToInt(number *ptr) {
+	int result = 0;
+	int resultTemp = 0;
+	char* num = ptr -> numString;
+	int len = (int)strlen(ptr -> numString);
+	
+	for (num; *num != '\0'; num++) {
+		result = result * 10 + (*num - '0');
+		if (resultTemp > result) {
+			return 1;
+		}
+		resultTemp = result;
+	}
+	
+	if (ptr -> negative == 1) {
+		result = result*-1;
+	} 	
+	ptr -> equiv = result;
+	return 0;
+}
+
+//return 1 for error;
+int binToInt(number* ptr) {
+	char* numB = ptr -> numString;
+	int result = 0;
+	int resultTemp = 0;
+	for (numB; *numB != '\0'; numB++) {
+		result = result * 2 + (*numB - '0');
+		if (resultTemp > result) {
+			return 1;
+		}
+		resultTemp = result;
+	}
+	
+	if (ptr -> negative == 1)
+		result = result*-1;
+	
+	ptr -> equiv = result;
+	
+	return 0;
+}
+
+int octToInt(number* ptr) {
+	char* numO = ptr -> numString;
+	int result = 0;
+	int resultTemp = 0;
+	for (numO; *numO != '\0'; numO++) {
+		result = result * 8 + (*numO - '0');
+		if (resultTemp > result) {
+			return 1;
+		}
+		resultTemp = result;
+	}
+	
+	if (ptr -> negative == 1)
+		result = result*-1;
+	
+	ptr -> equiv = result;
+	
+	return 0;
+}
+
+int hexToInt(number* ptr) {
+	char* numX = ptr -> numString;
+	int result = 0;
+	int resultTemp = 0;
+	int i = 0;
+	int y = 0;
+	int hexNum;
+	for (numX; *numX!= '\0'; numX++) {
+		i++;
+	}
+	
+	numX--;
+	for (y; y < i; y++) {
+		if (isdigit(*numX)) {
+			hexNum = *numX - '0';
+		}
+		else if (*numX == 'a' || *numX == 'A') {
+			hexNum = 10;
+		}
+		else if (*numX == 'b' || *numX == 'B') {
+			hexNum = 11;
+		}	
+		else if (*numX == 'c' || *numX == 'C') {
+			hexNum = 12;
+		}
+		else if (*numX == 'd' || *numX == 'D') {
+			hexNum = 13;
+		}
+		else if (*numX == 'e' || *numX == 'E') {
+			hexNum = 14;
+		}
+		else if (*numX == 'f' || *numX == 'F') {
+			hexNum = 15;
+		}
+		result = result + hexNum*(int)pow(16,y);
+		if (resultTemp > result) {
+			return 1;
+		}
+		resultTemp = result;
+		numX--;
+	}
+	
+	if (ptr -> negative == 1)
+		result = result*-1;
+	
+	ptr -> equiv = result;
+	
+	return 0;
+}
+
+//return 1 for error
+int convertToInt(number* ptr) {
+	switch(ptr -> type) {
+		case('b'): {
+			if (binToInt(ptr) == 1) 
+				return 1; 
+			else 
+				return 0;
+		}
+		case('o'): {
+			if (octToInt(ptr) == 1) 
+				return 1; 
+			else 
+				return 0;
+		}
+		case('d'): {
+			if (strToInt(ptr)==1)
+				return 1;
+			else
+				return 0;
+		}
+		case('x'): {
+			if (hexToInt(ptr) == 1)
+				return 1;
+			else
+				return 0;
+		}
+	}
+	return 0;
 }
 
 number* numCreate (char* str) {
 	number *ptr = (number*) malloc (sizeof(number));
-		
+	ptr -> equiv = 0;	
+	
 	if (str[0] == '-') {
 		ptr -> negative = 1;
 		ptr -> type = str[1];
@@ -170,22 +339,191 @@ number* numCreate (char* str) {
 		ptr -> type = str[0];
 		ptr -> numString = strdup(&str[1]);
 	}
-		
+	
+	if (convertToInt(ptr) == 1)
+		return NULL;
+	
 	return ptr;
+}
+
+int reverseStr(char* str) {
+   int i = strlen(str)-1;
+   int j = 0;
+   
+   char temp;
+   
+   while (j < i ) {
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		i--;
+		j++;
+   }
+   
+   return 0;
+}
+
+char* toBinary(int ans) {
+	char* result = (char*) malloc(sizeof(char)*33);
+	int i = 0;
+	int mem = ans;
+	
+	if (ans == 0) {
+		result[0] = '0';
+		result[1] = '\0';
+		return result;
+	}
+	
+	if (ans < 0)
+		ans = ans*-1;
+	
+	while (ans != 0) {
+		int b = ans%2;
+		printf("b is: %d\n",b);
+		result[i] = b + '0';
+		i++;
+		ans = ans/2;		
+	}
+	
+	if (mem < 0) {
+		result[i] = '-';
+		i++;
+	}
+	
+	result[i] = '\0';
+	
+	reverseStr(result);
+	
+	return result;
+}
+
+char* toOctal(int ans) {
+	char* result = (char*) malloc(sizeof(char)*33);
+	int i = 0;
+	int mem = ans;
+	
+	if (ans == 0) {
+		result[0] = '0';
+		result[1] = '\0';
+		return result;
+	}
+	
+	if (ans < 0)
+		ans = ans*-1;
+	
+	while (ans != 0) {
+		int b = ans%8;
+		result[i] = b + '0';
+		i++;
+		ans = ans/8;		
+	}
+	
+	if (mem < 0) {
+		result[i] = '-';
+		i++;
+	}
+	
+	result[i] = '\0';
+	
+	reverseStr(result);
+	
+	return result;
+}
+
+char* toHexa(int ans) {
+	char* result = (char*) malloc(sizeof(char)*33);
+	int i = 0;
+	int mem = ans;
+	
+	if (ans == 0) {
+		result[0] = '0';
+		result[1] = '\0';
+		return result;
+	}
+	
+	if (ans < 0)
+		ans = ans*-1;
+	
+	while (ans != 0) {
+		int b = ans%16;
+		if (b == 10) {
+			result[i] = 'a';
+			printf("the letter assigned is %c\n", result[i]);
+		}
+		else if (b == 11) {
+			result[i] = 'b';
+		}
+		else if (b == 12) {
+			result[i] = 'c';
+		}
+		else if (b == 13) {
+			result[i] = 'd';
+		}
+		else if (b == 14) {
+			result[i] = 'e';
+		}
+		else if (b == 15) {
+			result[i] = 'f';
+		}
+		else {
+			result[i] = b + '0';
+		}
+		i++;
+		ans = ans/16;		
+	}
+	
+	if (mem < 0) {
+		result[i] = '-';
+		i++;
+	}
+	
+	result[i] = '\0';
+	reverseStr(result);
+	
+	return result;
+}
+
+int solveEqn(char op, char type, number* num1, number* num2) {
+	int ansTemp;
+	int x = num1 -> equiv;
+	int y = num2 -> equiv;
+		
+	if (op == '+') {
+		ansTemp = x + y;
+	}
+	if (op == '-') {
+		ansTemp = x - y;
+	}
+	if (op == '*') {
+		ansTemp = x * y;
+	}
+	
+	return ansTemp;	
+}
+
+char* convertAns (char type, int ans) {
+	if (type == 'b' || type == 'B')
+		return toBinary(ans);
+	if (type == 'x' || type == 'X')
+		return toHexa(ans);
+	if (type == 'o' || type == 'O')
+		return toOctal(ans);
 }
 /*main will take in five arguments to perform a calculation called by user. They will be able to input numbers 
  * in octal, hexadecimal, decimal and binary. They will also be able to output answers in one of those four types. 
  * negative numbers maybe used.  */
 int main (int argc, char** argv) {
 	int result;
+	char* resultStr;
 	int stop = 0;
-	
+				
 	if (argc < 5) {
 		fprintf(stdout,"Not enough arguments!\n");
 		return 0;
 	}
 	else if (argc > 5) {
 		fprintf(stdout,"Too many arguments!\n");
+		printf("%d\n",argc);
 		return 0;
 	}
 	
@@ -219,7 +557,36 @@ int main (int argc, char** argv) {
 	number* num1 = numCreate(argv[2]);
 	number* num2 = numCreate(argv[3]);
 	
-	//result = solveEqn(argv[1][0],num1,num2);    //temporary 
+	if (num1 == NULL && num2 == NULL) {
+		printf("First and second value is greater than 32bits\n");
+		return 0;
+	}
+	else if (num1 == NULL) {
+		printf("First value is greater than 32bits\n");
+		return 0;
+	}
+	else if (num2 == NULL) {
+		printf("Second value is greater than 32bits\n");
+		return 0;
+	}
+	
+	printf("Value stored in num1: %d\n",num1 -> equiv);
+	printf("Value stored in num1: %d\n",num2 -> equiv);
+	
+	result = solveEqn(argv[1][0],argv[4][0],num1,num2);  
+	
+	printf("Decimal answer is: %d\n",result);
+	
+	if (argv[4][0] == 'd') {
+		printf("RESULT: %c%d\n",argv[4][0],result);
+	}
+	else {
+		resultStr = convertAns(argv[4][0],result);
+		printf("RESULT: %c%s\n",argv[4][0],resultStr);
+		free (resultStr);
+	}
+	
+	//delete(num1,num2);
 	
 	return 0;
 }
